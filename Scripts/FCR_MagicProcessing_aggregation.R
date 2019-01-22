01d98aa21d938de9560f239670832d426a858ae4
bethanybookout <38041971+bethanybookout@users.noreply.github.com>
2019-01-22 16:48:51 -0500

    script for preliminary processing Magic 2018

---
+++ Scripts/FCR_MagicProcessing_aggregation.R
@@ -0,0 +1,8 @@
+#Script to work with Magic data from Fall 2018
+
+#Let's see if anything is worth anything
+
+#Tasks:
+#1. Post acid cleaning (October 10th ish), plot pre and post cleaning air valve
+#2. Plot DI valve
+#3. Plot Surface valve
