03ba003a171a314730078188563284736ec99200
bethany.bookout <bethanyb18@vt.edu>
2019-01-22 17:05:57 -0500

    updated tasks, added future notes

--- Scripts/FCR_MagicProcessing_aggregation.R
+++ Scripts/FCR_MagicProcessing_aggregation.R
@@ -3,17 +3,24 @@
 #Let's see if anything is worth anything
 
 #Tasks:
-#1. Post acid cleaning (October 10th ish), plot pre and post cleaning air valve
-#2. Plot DI valve
-#3. Plot Surface valve
-#4. Compare magic data to DO + temp data at 1, 5, and 9m
+#1. Load in data in legit way
+#2. Post acid cleaning (October 10th ish), plot pre and post cleaning air valve (what is the best absorbance col to use?)
+#3. Plot DI valve
+#4. Plot Surface valve
+#5. Compare magic data to DO + temp data at 1, 5, and 9m
 
 #loading in data
 setwd('./MagicData')
 
 ###putting in met merge script as place holder
-metfiles<-list.files(path=getwd())
+magicrawfiles<-list.files(path=getwd())
+
+#create list with files after cleaning protocol
+magicfiles<-list.files(path=".", pattern = "810*")
+
 obs<-read.csv(file=metfiles[1],skip=4,header=FALSE) #read in first file
+
+#paste in actual 
 names(obs) = c("TIMESTAMP","RECORD","BattV","PTemp_C","PAR_Den_Avg","PAR_Tot_Tot","BP_kPa_Avg","AirTC_Avg","RH","Rain_mm_Tot","WS_ms_Avg","WindDir","SR01Up_Avg","SR01Dn_Avg","IR01UpCo_Avg","IR01DnCo_Avg","Albedo_Avg")
 units = c("TS","RN","Volts","Deg C","umol/s/m^2","mmol/m^2","kPa","Deg C","%","mm","meters/second","degrees","W/m^2","W/m^2","W/m^2","W/m^2","W/m^2") #creates list of units, skipped in line above
 obs$TIMESTAMP = as.POSIXct(obs$TIMESTAMP)
