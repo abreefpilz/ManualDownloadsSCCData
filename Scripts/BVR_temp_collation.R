b6ae021d3256cc28b9d9830070355f433c4e7dcc
bethanybookout <bethanyb18@vt.edu>
2020-02-12 11:27:56 -0500

    trying to figure out how to read in EXO data with pkg

--- Scripts/BVR_temp_collation.R
+++ Scripts/BVR_temp_collation.R
@@ -5,6 +5,7 @@
 
 library(plyr)
 library(readr)
+library(driftR) #package to read in EXO and hobo data frames
 
 #load in all hobo temp string data
 #how do I want to do this?? by depth? then merge by time?
@@ -17,3 +18,9 @@ dat_csv
 
 #load minidot DO data
 DO_5m=read.table("BVR_SensorString/BVR_DO_5m_20181206_20191206.TXT", sep=",", skip = 6, header = T)
+
+
+#load EXO data
+EXO_BVR=read.table("BVR_SensorString/BVR_1_5m_20191007_20191212.csv", sep= ",", skip = 8, header = T, blank.lines.skip = T)
+#what is wrong with this loading?????
+EXO_BVR=dr_read("BVR_SensorString/BVR_1_5m_20191007_20191212.csv", instrument = "EXO")
