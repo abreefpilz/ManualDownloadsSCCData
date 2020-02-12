f9aacfea238ea95dea21b5937096b0064ae24ccd
bethanybookout <bethanyb18@vt.edu>
2020-02-12 11:02:29 -0500

    do loading, notes for temp loading

--- Scripts/BVR_temp_collation.R
+++ Scripts/BVR_temp_collation.R
@@ -6,9 +6,14 @@
 library(plyr)
 library(readr)
 
+#load in all hobo temp string data
+#how do I want to do this?? by depth? then merge by time?
 mydir = "BVR_SensorString"
-myfiles = list.files(path=mydir, pattern="*.csv", full.names=TRUE)
+myfiles = list.files(path=mydir, pattern="Hobo*", full.names=TRUE)
 myfiles
 
 dat_csv = ldply(myfiles, read_csv)
 dat_csv
+
+#load minidot DO data
+DO_5m=read.table("BVR_SensorString/BVR_DO_5m_20181206_20191206.TXT", sep=",", skip = 6, header = T)
