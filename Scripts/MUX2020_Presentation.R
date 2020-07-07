34a8d709cae805688eb956dddd840ac7730a4ee9
bethanybookout <bethanyb18@vt.edu>
2020-07-07 12:01:03 -0400

    new script for presentation!

---
+++ Scripts/MUX2020_Presentation.R
@@ -0,0 +1,10 @@
+#Magic Mux 2020 Data Presentation
+#Authors: Bethany Bookout, Nick Hammond
+
+#Outline
+
+#1. 1.6 scan data
+#a. Load in data
+#b. Put all files tog
+#2. 1.6 scan maintenance log?
+#3. Mux 2020 data
