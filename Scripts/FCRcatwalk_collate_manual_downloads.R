8b463f2fa9f39987394a3f6011ddaa892b7f3cd4
Carey Lab <careylabvt@gmail.com>
2021-06-09 22:49:47 -0400

    Collated FCR files

--- Scripts/FCRcatwalk_collate_manual_downloads.R
+++ Scripts/FCRcatwalk_collate_manual_downloads.R
@@ -32,6 +32,31 @@ for(k in 1:length(myfiles)){
 #get rid of duplicates
 fcrdata=fcrdata3[!duplicated(fcrdata3$TIMESTAMP), ]
 
+#put manual data from FCR pressure sensor into a file 
+mydir2 = "CR6_Files/Pressure_Sensor"
+myfiles2 = list.files(path=mydir2, pattern="", full.names=TRUE)#list the files from BVR platform
+
+#taking out the the Temp Test files
+#myfilesBVR <- myfiles[ !grepl("CR6_BVR_TempTest*", myfiles) ]#exclude the Temp test data
+
+#create dataframe for the for loop
+presdata3<-""
+
+#combine all of the files into one data sheet, have to come back and fix this loop
+for(j in 1:length(myfiles2)){
+  presheader2<-read.csv(myfiles2[j], skip=1, as.is=T) #get header minus wonky Campbell rows
+  presdata2<-read.csv(myfiles2[j], skip=4, header=F) #get data minus wonky Campbell rows
+  names(presdata2)<-names(presheader2) #combine the names to deal with Campbell logger formatting
+  presdata3=rbind(presdata2, presdata3)
+}
+
+#get rid of duplicates
+presdata=presdata3[!duplicated(presdata3$TIMESTAMP), ]
+
+#merge the catwalk files and the pressure sensor
+
+FCR=merge(fcrdata,presdata, all.x=T)
+
 #create CSV of manual downloads which can be combined with Github files to fill in missing gaps
 
-write.csv(bvrdata, "BVRplatform_manual_2020.csv")
+write.csv(FCR, "FCRcatwalk_manual_2021.csv")
