165e4c1d90894d1f0677ddce159d43f077fabaf5
Nick Hammond <hammondnw@vt.edu>
2022-11-10 18:09:53 -0500

    EDI stuff

--- MagicData/MUX/EDI Publishing/MakeEML_MUX.R
+++ MagicData/MUX/EDI Publishing/MakeEML_MUX.R
@@ -23,9 +23,9 @@ library(EMLassemblyline)
 library(tidyverse)
 
 #Step 1: Create a directory for your dataset, here "EDI_publishing" within the main repo
-setwd("./MagicData/MUX/")
+
 #Step 2: Move your dataset to the directory
-# 'EDI Publishing'
+
 
 #now set files to the correct directory
 setwd("./MagicData/MUX/EDI Publishing/")
@@ -36,11 +36,24 @@ setwd("./MagicData/MUX/EDI Publishing/")
 #Step 4: Identify the types of data in your dataset
 
 #Step 5: Import the core metadata templates to your local directory
-import_templates(path = "C:/Users/hammo/Documents/Magic Sensor PLSR/ManualDownloadsSCCData/MagicData/MUX/EDI Publishing/",
-                 license = "CCBY",
-                 data.files = c("MUX_FP_TS.csv", 
-                                "MUX_predictions_boot.csv",
-                                ))
+template_core_metadata(path = "C:/Users/hammo/Documents/Magic Sensor PLSR/ManualDownloadsSCCData/MagicData/MUX/EDI Publishing/",
+                       license = "CCBY",
+                       file.type = ".txt",
+                       write.file = TRUE)
+
+template_table_attributes(path = "C:/Users/hammo/Documents/Magic Sensor PLSR/ManualDownloadsSCCData/MagicData/MUX/EDI Publishing/",
+                          data.path = "C:/Users/hammo/Documents/Magic Sensor PLSR/ManualDownloadsSCCData/MagicData/MUX/EDI Publishing/",
+                          data.table = c("MUX_FP_TS.csv", 
+                                         "MUX_predictions_boot.csv"),
+                          write.file = TRUE)
+#we want empty to be true for this because we don't include lat/long
+#as columns within our dataset but would like to provide them
+template_geographic_coverage(path = "C:/Users/hammo/Documents/Magic Sensor PLSR/ManualDownloadsSCCData/MagicData/MUX/EDI Publishing/",
+                             data.path = "C:/Users/hammo/Documents/Magic Sensor PLSR/ManualDownloadsSCCData/MagicData/MUX/EDI Publishing/",
+                             data.table = c("MUX_FP_TS.csv", 
+                                            "MUX_predictions_boot.csv"),
+                             empty = TRUE,
+                             write.file = TRUE)
 
 # Edit each of these imported template files for your current data package upload, by copying and 
 # pasting the relevant information from the EDI_metadata_template you prepared
@@ -84,83 +97,108 @@ view_unit_dictionary()
 #grab attribute names and definitions from your metadata word document
 #for units....
 # View and search the standard units dictionary
-#view_unit_dictionary()
+view_unit_dictionary()
 #put flag codes and site codes in the definitions cell
 #force reservoir to categorical
+
 
 #Step 13: Close files
 #if all your files aren't closed, sometimes functions don't work
 
 #Step 14: Categorical variables
-# View documentation for this function
-#?define_catvars
-
 # Run this function for your dataset
-#define_catvars(path = "/Users/cayelan/Dropbox/ComputerFiles/SCC/FCR-GLM/FCR_2013_2019GLMHistoricalRun_GLMv3beta/EDI_publishing")
+#THIS WILL ONLY WORK once you have filled out the attributes_chemistry.txt and
+#identified which variables are categorical
+template_categorical_variables(path = "C:/Users/hammo/Documents/Magic Sensor PLSR/ManualDownloadsSCCData/MagicData/MUX/EDI Publishing/",
+                               data.path = "C:/Users/hammo/Documents/Magic Sensor PLSR/ManualDownloadsSCCData/MagicData/MUX/EDI Publishing/",
+                               write.file = TRUE)
+
 #open the created value IN A SPREADSHEET EDITOR and add a definition for each category
 
 #Step 15: Geographic coverage
-#copy-paste the bounding_boxes.txt file that is Carey Lab specific into your working directory
+#copy-paste the bounding_boxes.txt file (or geographic_coverage.txt file) that is Carey Lab specific into your working directory
 
-#Step 16: Make EML
-# View documentation for this function
-?make_eml
-
-## Step XXX: Obtain a package.id. ####
+## Step 16: Obtain a package.id. ####
 # Go to the EDI staging environment (https://portal-s.edirepository.org/nis/home.jsp),
-# then login using one of the Carey Lab usernames and passwords. 
+# then login using one of the Carey Lab usernames and passwords.
 
 # Select Tools --> Data Package Identifier Reservations and click 
 # "Reserve Next Available Identifier"
 # A new value will appear in the "Current data package identifier reservations" 
-# table (e.g., edi.123.1)
+# table (e.g., edi.123)
 # Make note of this value, as it will be your package.id below
 
-#for this FCR-GLM-AED data package, identifier is 'edi.471.1', then .2
-# updated for manuscript revision to be 471.3!
 
-## Step XXX: Make EML metadata file using the EMLassemblyline::make_eml() command ####
-# For modules that contain only zip folders, modify and run the following 
-# ** double-check that all files are closed before running this command! **
+#Step 17: Make EML
+# View documentation for this function
+?make_eml
 
-# You will need to modify the following lines to match your current module: 
-# path: set to your computer's FULL file path, not github directory!
-# dataset.title: Update
-# zip.dir: Change the name of the module files zip folder
-# temporal.coverage: Update the dates
-# package.id: enter the ID you obtained in Step 6
-make_eml(path = "/Users/cayelan/Dropbox/ComputerFiles/SCC/FCR-GLM/FCR_2013_2019GLMHistoricalRun_GLMv3beta/EDI_publishing",
-         data.path = "/Users/cayelan/Dropbox/ComputerFiles/SCC/FCR-GLM/FCR_2013_2019GLMHistoricalRun_GLMv3beta/EDI_publishing",
-         eml.path = "/Users/cayelan/Dropbox/ComputerFiles/SCC/FCR-GLM/FCR_2013_2019GLMHistoricalRun_GLMv3beta/EDI_publishing",
-         dataset.title = "General Lake Model-Aquatic EcoDynamics model parameter set for Falling Creek Reservoir, Vinton, Virginia, USA 2013-2019",
-         data.table = c("FCR_GLM_NLDAS_010113_123119_GMTadjusted.csv", 
-                        "FCR_spillway_outflow_SUMMED_WeirWetland_2013_2019_20200615.csv",
-                        "FCR_SSS_inflow_2013_2019_20200701_allfractions_2DOCpools.csv",
-                        "FCR_weir_inflow_2013_2019_20220104_allfractions_2poolsDOC.csv",
-                        "FCR_wetland_inflow_2013_2019_20220104_allfractions_2DOCpools.csv"),
-         data.table.name = c("FCR_GLM_NLDAS_010113_123119_GMTadjusted.csv", 
-                             "FCR_spillway_outflow_SUMMED_WeirWetland_2013_2019_20200615.csv",
-                             "FCR_SSS_inflow_2013_2019_20200701_allfractions_2DOCpools.csv",
-                             "FCR_weir_inflow_2013_2019_20220104_allfractions_2poolsDOC.csv",
-                             "FCR_wetland_inflow_2013_2019_20220104_allfractions_2DOCpools.csv"),
-         data.table.description = c('Meteorological driver data',
-                                    'Outflow driver data',
-                                    'Oxygenation system submerged inflow driver data',
-                                    'Tunnel Branch stream inflow driver data',
-                                    'Falling Creek stream inflow driver data'),
-         other.entity = c("model_configuration_nml_files.zip"),
-         other.entity.name = c("model_configuration_nml_files.zip"),
-         other.entity.description =c("GLM-AED nml configuration files"),
-         temporal.coverage = c("2013-05-13", "2019-12-31"),
-         #geographic.description = c("Falling Creek Reservoir, Vinton, Virginia, USA"),
-         #geographic.coordinates = c('37.309589', '-79.836009', '37.302660', '-79.839249'), #N, E, S, W
-         maintenance.description = "completed", 
-         user.id = "ccarey",
-         user.domain = 'EDI',
-         package.id = "edi.1127.1") # Put your package.id here, followed by .1 (for 1st version)
+# Run this function
+make_eml(
+  path = "C:/Users/hammo/Documents/Magic Sensor PLSR/ManualDownloadsSCCData/MagicData/MUX/EDI Publishing/",
+  data.path = "C:/Users/hammo/Documents/Magic Sensor PLSR/ManualDownloadsSCCData/MagicData/MUX/EDI Publishing/",
+  eml.path = "C:/Users/hammo/Documents/Magic Sensor PLSR/ManualDownloadsSCCData/MagicData/MUX/EDI Publishing/",
+  dataset.title = "Time series of in situ Uv-vis absorbance spectra and high-frequency predictions of total and soluble Fe and Mn concentrations measured at multiple depths in Falling Creek Reservoir (Vinton, VA, USA) in 2020-2021",
+  temporal.coverage = c("2020-10-16", "2021-06-21"),
+  maintenance.description = 'ongoing',
+  data.table = c("MUX_FP_TS.csv", 
+                 "MUX_predictions_boot.csv"),
+  data.table.description = c("time series of uv-vis absorbance spectra",
+                             "time series of predicted Fe and Mn concentrations"),
+  other.entity = c("1_Overlaps_MUX.R","2_PLSR_MUX_bootstrap.R"),
+  other.entity.description = c("Data Processing Script","PLSR Analysis Script"),
+  user.id = 'mschreib',
+  user.domain = 'EDI',
+  package.id = 'edi.974.1')
 
-#note: once the EML is validated, THEN need to edit to add in abstract, keywords, and methods.
-#see edi.456.3.xml (Ice at FCR- as an example!)
+## Step 8: Check your data product! ####
+# Return to the EDI staging environment (https://portal-s.edirepository.org/nis/home.jsp),
+# then login using one of the Carey Lab usernames and passwords. 
+
+# Select Tools --> Evaluate/Upload Data Packages, then under "EML Metadata File", 
+# choose your metadata (.xml) file (e.g., edi.270.1.xml), check "I want to 
+# manually upload the data by selecting files on my local system", then click Upload.
+
+# Now, Choose File for each file within the data package (e.g., each zip folder), 
+# then click Upload. Files will upload and your EML metadata will be checked 
+# for errors. If there are no errors, your data product is now published! 
+# If there were errors, click the link to see what they were, then fix errors 
+# in the xml file. 
+# Note that each revision results in the xml file increasing one value 
+# (e.g., edi.270.1, edi.270.2, etc). Re-upload your fixed files to complete the 
+# evaluation check again, until you receive a message with no errors.
+
+## Step 9: PUBLISH YOUR DATA! ####
+# Reserve a package.id for your error-free data package. 
+# NEVER ASSIGN this identifier to a staging environment package.
+# Go to the EDI Production environment (https://portal.edirepository.org/nis/home.jsp)
+# and login using the ccarey (permanent) credentials. 
+
+# Select Tools --> Data Package Identifier Reservations and click "Reserve Next 
+# Available Identifier". A new value will appear in the "Current data package 
+# identifier reservations" table (e.g., edi.518)
+# This will be your PUBLISHED package.id
+
+# In the make_eml command below, change the package.id to match your 
+# PUBLISHED package id. This id should end in .1 (e.g., edi.518.1)
+
+# ALL OTHER entries in the make_eml() command should match what you ran above,
+# in step 7
+
+make_eml(
+  path = "C:/Users/hammo/Documents/Magic Sensor PLSR/ManualDownloadsSCCData/MagicData/MUX/EDI Publishing/",
+  data.path = "C:/Users/hammo/Documents/Magic Sensor PLSR/ManualDownloadsSCCData/MagicData/MUX/EDI Publishing/",
+  eml.path = "C:/Users/hammo/Documents/Magic Sensor PLSR/ManualDownloadsSCCData/MagicData/MUX/EDI Publishing/",
+  dataset.title = "Time series of total and soluble iron and manganese concentrations from Falling Creek Reservoir and Beaverdam Reservoir in southwestern Virginia, USA from 2014 through 2021",
+  temporal.coverage = c("2014-04-01", "2021-12-06"),
+  maintenance.description = 'ongoing',
+  data.table = "Metals_2014_2021.csv",
+  data.table.description = "Reservoir iron and manganese chemistry dataset",
+  other.entity = "Metals_QAQC.R",
+  other.entity.description = "QAQC script",
+  user.id = 'mschreib',
+  user.domain = 'EDI',
+  package.id = 'edi.455.6')
 
 # Once your xml file with your PUBLISHED package.id is Done, return to the 
 # EDI Production environment (https://portal.edirepository.org/nis/home.jsp)
